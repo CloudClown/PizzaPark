fd2b88fb9212e2180f14f0b6020dffa374da9939
tahmid <radon1441@gmail.com>
2022-01-10 10:37:29 +0600

    form left with custom css

--- src/Component/Register.js
+++ src/Component/Register.js
@@ -4,11 +4,14 @@ import regImg from '../img/reg.png';
 // mui
 import Input from '@mui/material/Input';
 import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import SigninIcon from '@mui/icons-material/Login';
 
 const Register = () => {
   return (
     <>
       <div className="container">
+        <div className="reg-background"></div>
         <Grid
           container
           direction="row"
@@ -16,18 +19,31 @@ const Register = () => {
           alignItems="center"
         >
           <Grid item md={6}>
-            <div className="circle">here will be circle</div>
-            <div className="reg-img">
-              <img src={regImg} alt="register" />
+            <div className="form">
+              <Grid
+                container
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+              >
+                <form noValidate autoComplete="off">
+                  <Grid item md={6}>
+                    <Input placeholder="Email" required />
+                  </Grid>
+                  <Grid item md={6}>
+                    <Input placeholder="Password" required />
+                  </Grid>
+                </form>
+              </Grid>
             </div>
           </Grid>
 
           <Grid item md={6}>
-            <div className="form">
-              <form noValidate autoComplete="off">
-                <Input placeholder="Email" required />
-                <Input placeholder="Password" required />
-              </form>
+            <Button variant="contained" startIcon={<SigninIcon />}>
+              Send
+            </Button>
+            <div className="reg-img">
+              <img src={regImg} alt="register" className="register-img" />
             </div>
           </Grid>
         </Grid>
