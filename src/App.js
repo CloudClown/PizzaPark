a49b7765fadb4172bb16190e0612387ecf5876cb
tahmid <radon1441@gmail.com>
2022-02-17 00:06:25 +0600

    project pages

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@
 import { Route, Routes } from 'react-router-dom';
 // Files
 import './App.css';
-import HeaderPage from './Component/HeaderPage';
-import Register from './Component/Register';
-import Login from './Component/Login';
+import LandingPage from './LandingPage/LandingPage';
+import SignUp from './Component/SignUp';
+import SignIn from './Component/SignIn';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route exact path="/" element={<HeaderPage />} />
-        <Route exact path="/register" element={<Register />} />
-        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/" element={<LandingPage />} />
+        <Route exact path="/register" element={<SignUp />} />
+        <Route exact path="/login" element={<SignIn />} />
       </Routes>
     </div>
   );
---
+++ src/Component/SignUp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const SignUp = () => {
+
+  return (
+    <>
+      <h1>Sign-up</h1>
+    </>
+  );
+};
+
+export default SignUp;
